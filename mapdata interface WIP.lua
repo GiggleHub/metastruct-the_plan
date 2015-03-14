ffdb5ced43a943960091a52f811c1496ed47e498
Python1320 <python1320@users.noreply.github.com>
2015-03-15 01:18:58 +0200

    inventory mapdata interface todos

--- mapdata interface WIP.lua
+++ mapdata interface WIP.lua
@@ -1,56 +1,76 @@
-local META1={}
-local META2={}
-function META:update()
-	
-end
-function META1:add(tt)
-	local t=self[1]
-	t[#t+1]=tt
-end
-function META:iter()
-	
-end
+-- The plan for new mapdata interface --
+-- TODO: Add "relative to" to all offsets (maybe change to mapoffset+pos tuples)
+-- TODO: Finish all use cases
+-- TODO: Implement system
+-- TODO: Use the system
+-- PROBLEM: data removal, what happens to live maplinkage, discard/handle?
 
-local m=ms.mapdata
-m={
-	
-	triggers={
-		{name="bb",min=Vector(3,3,3),max=Vector(3,4,5)},
-		{name="banni",min=Vector(3,3,3),max=Vector(3,4,5)},
-		{name="ii",min=Vector(3,3,3),max=Vector(3,4,5)},
-	},
-	hospitalpos=ector(0,0,0),
-	wanderer_sitpos={
-	{Vector(),Angle()}
-	}
-}
+-- POINT OF VIEWS
 
 
-m.triggers:add{name='bani',min=Vector(3,3,3),max=Vector(4,4,4)}
+-- Trigger
 
-function ENT:OnMapData(data)
-	self:SetPos(data.min*0.5+data.max*0.5)
-	self:SetOBBWorld(data.min,data.max)
-end
-
-local function CreateTriggers()
-	
-	for data in m.triggers:iter() do
+	mapdata={
 		
-		local ent=ents.Create'base_lua_trigger'
-		data:link(ent)
+		triggers={
+			{name="saunatv",min=Vector(3,3,3),max=Vector(3,4,5),mapoffset="sauna"},
+			{name="banni",min=Vector(3,3,3),max=Vector(3,4,5),mapoffset="jail"},
+			{name="ii",min=Vector(3,3,3),max=Vector(3,4,5),mapoffset=false},
+		}
+
+	-- lua trigger creation, map based triggers maybe too
+	local function CreateTriggers()
 		
-	end
+		for data in next,m.triggers do
+			
+			local ent=ents.Create'base_lua_trigger'
+			data:MapLink(ent)
+			ent:Spawn()
+			
+		end
 
-end
-
-
-for data in m.triggers:iter() do
-	
-	if data.name=="bani" then
-		data.min=Vector(0,0,0)
-		data.max=Vector(1,1,1)
-		data:update()
 	end
 	
-end
+	-- called each time the data changes and on link
+	function TRIGGER:OnMapLink(data)
+		self:SetPos(data.min*0.5+data.max*0.5)
+		self:SetOBBWorld(data.min,data.max)
+	end
+
+	
+
+-- Lua screens
+
+	mapdata={
+		
+		lscreens={
+			{"motd",pos,ang,"lobby1"},
+		}
+
+	-- lua trigger creation, map based triggers maybe too
+	local function CreateLuaScreens()
+		
+		for data in next,m.lscreens do
+			
+			local ent=ents.Create'lua_screen'
+			data:MapLink(ent)
+			ent:Spawn()
+			
+		end
+
+	end
+	
+	-- called each time the data changes and on link
+	function SCREEN:OnMapLink(data)
+		ent:SetPlace(data[1])
+		ent:SetPos(data[2])
+		ent:SetAngles(data[3])
+	end
+
+-- wanderer spawn
+-- wanderer sitting
+-- wanderer waypoints
+
+-- mine positions
+
+-- random positions
